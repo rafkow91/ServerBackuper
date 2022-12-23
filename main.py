4c1f5d97b6fa16242aac4e1c4a4602afd2710ac3
Rafał Kowalski <rafkow91@yahoo.com>
2022-12-23 11:25:47 +0100

    change config files (json -> yaml), add getting files from remote servers

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 """ Server Backuper -> make backups using ssh connection    """
 from json import load
+import yaml
 
 from paramiko.ssh_exception import NoValidConnectionsError
 
 from modules.ssh import SSHContextManager
 
 if __name__ == '__main__':
-    with open('config.json', mode='r') as file:
-        servers = load(file)
-    # with SSHContextManager()
-    print(servers)
-    for server in servers:
-        try:
-            with SSHContextManager(username=server['username'], host=server['host'], ssh_key_path=server.get('ssh_key', None), password=server.get('password', None)):
-                print(f'connect OK (IP: {server["host"]})')
-        except NoValidConnectionsError as e:
-            print(e)
+    with open('config.yml', mode='r') as file:
+        servers = yaml.safe_load_all(file)
+
+        for server in servers:
+            try:
+                with SSHContextManager(username=server['username'], host=server['host'], ssh_key_path=server.get('ssh_key', None), password=server.get('password', None)) as ssh:
+                    print(f'connect OK (IP: {server["host"]})')
+
+                    ssh.get_files(['D:\\test.txt'])
+            except NoValidConnectionsError as e:
+                print(str(e)[13:])
--- modules/ssh.py
+++ modules/ssh.py
@@ -1,20 +1,41 @@
+''' Remote client works on SSH'''
 import os
 import sys
 
-import getpass
+from getpass import getpass
 from paramiko import SSHClient, AutoAddPolicy, AuthenticationException
+from scp import SCPClient
 
 USER = os.environ.get('USERNAME')
 
 DEFAULT_KNOWS_HOST_PATHS = {
     'linux': f'/home/{USER}/.ssh/known_hosts',
-    # 'windows': f'C:\users\{USER}\ssh',
-    # 'darwin': f'/users/{USER}/.ssh/known_hosts'
+    'windows': f'C:\\users\\{USER}\\ssh',
+    'darwin': f'/users/{USER}/.ssh/known_hosts'
 }
 
 
 class SSHContextManager:
-    def __init__(self, host: str, username: str, ssh_key_path: str = None, password: str = None, port: int = 22, known_hosts_path: str = None) -> None:
+    '''
+    > This function is used to get files from a remote server
+
+    :param host: The hostname or IP address of the server
+    :type host: str
+    :param username: The username to use for the SSH connection
+    :type username: str
+    :param ssh_key_path: The path to the SSH key file
+    :type ssh_key_path: str
+    :param password: The password for the user
+    :type password: str
+    :param port: The port to connect to on the remote server, defaults to 22
+    :type port: int (optional)
+    :param known_hosts_path: The path to the known_hosts file. If not provided, it will be set to
+    the default path for the current OS
+    :type known_hosts_path: str
+    '''
+
+    def __init__(self, host: str, username: str, ssh_key_path: str = None,
+                 password: str = None, port: int = 22, known_hosts_path: str = None) -> None:
         self.client = SSHClient()
         self.host = host
         self.known_hosts_path = known_hosts_path
@@ -49,3 +70,12 @@ class SSHContextManager:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.client.close()
+
+    def get_files(self, files_paths: list[str]):
+        '''
+        Getting files from the server.
+        '''
+        scp = SCPClient(self.client.get_transport())
+        for file_path in files_paths:
+            scp.get(file_path)
+        scp.close()
